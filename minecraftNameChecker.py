74e213a46863bfa76d83f20a221819fb73ac86be
Takahashi Oto <77091655+takaha4010@users.noreply.github.com>
2021-05-05 08:03:11 +0900

    Update minecraftNameChecker.py

--- minecraftNameChecker.py
+++ minecraftNameChecker.py
@@ -16,9 +16,11 @@ while True:
 
     res = soup.find("div",class_="my-1")
     if(res!=None) :
+        //通常の処理
         print(res.text)
         print('"'+wd+'"'+"調査完了")
     else :
+        //アクセスのし過ぎで、IPアドレスが一時的に制限された時の処理です。
         time.sleep(5)
         r = requests.get(url+wd)
         soup = BeautifulSoup(r.content, "html.parser")
